1b0ab07e836d008ca15f5a91c7a828f5b6b1d17e
ericsei <ericsei@dell.home>
2019-08-23 14:43:59 -0400

    WIP: added Authentication Middlewares and Project Folder Scaffolding

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
-//User Schema
+
+const mongoose = require('mongoose');
+
+const userShema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('User', userShema);
