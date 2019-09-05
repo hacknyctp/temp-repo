850a726777ceb54d50a41cab5b8e545a2b66d86a
Mark <markgoldstein12@gmail.com>
2019-09-04 21:22:37 -0400

    vim

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ app.use(express.json({ extended: false })); //Now we can accept body data
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.get("/api", function(req, res) {
-  res.send("Welcome to the API");
+  return res.status(200).json({
+    msg: "Welcome to the API"
+  });
 });
 app.get("/", function(req, res) {
   res.send(
     "Go to https://documenter.getpostman.com/view/7764095/SVfNv9Uc?version=latest  to see the API use case examples "
   );
+  //Put jwt into HTTP only cookie so js cant access it
+  //Dont use local storage for jwt
 });
 // // Serve static assets in production
 // if (process.env.NODE_ENV === "production") {
